e8d3c3bfab714c5f5b2d92a8a81c6a7a4297d8ec
Eddy Reyes <ereyes01@users.noreply.github.com>
2015-01-14 16:03:16 -0600

    Add test flask app fixtures

--- test_hello.py
+++ test_hello.py
@@ -1,4 +1,18 @@
-def test_hello():
+import pytest
+import hello_app
+
+
+@pytest.fixture
+def app():
+    return hello_app.app
+
+
+@pytest.fixture
+def test_client(app):
+    return app.test_client()
+
+
+def test_hello(test_client):
     """
     GIVEN: A flask hello app
     WHEN: I GET the hello/ route
