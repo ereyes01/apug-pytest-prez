52071bcc07b579bf349b76be38bfc5c07600335e
Eddy Reyes <ereyes01@users.noreply.github.com>
2015-01-14 17:34:49 -0600

    Add test and mock for hacker news request

--- test_hello.py
+++ test_hello.py
@@ -2,6 +2,24 @@ import pytest
 import hello_app
 
 
+MOCK_ENCODING = "mock-encoding"
+
+
+class MockEncodingResponse:
+    def __init__(self):
+        self.headers = {"Content-Encoding": MOCK_ENCODING}
+
+
+def _mock_get(url):
+    assert url == "https://news.ycombinator.com"
+    return MockEncodingResponse()
+
+
+@pytest.fixture
+def mock_encoding_request(monkeypatch):
+    monkeypatch.setattr("requests.get", _mock_get)
+
+
 @pytest.fixture
 def app():
     return hello_app.app
@@ -20,3 +38,14 @@ def test_hello(test_client):
     """
     response = test_client.get("/hello")
     assert response.data.decode("utf-8") == "hello world"
+
+
+def test_encoding_header(test_client, mock_encoding_request):
+    """
+    GIVEN: A flask hello app
+           A mock request handler
+    WHEN: I GET the /hacker_news_encoding route
+    THEN: The response should be the expected Content-Encoding
+    """
+    response = test_client.get("/hacker_news_encoding")
+    assert response.data.decode("utf-8") == MOCK_ENCODING
