bd8e62509bd5b09f0acd15152f2dc206ec723e41
Eddy Reyes <ereyes01@users.noreply.github.com>
2015-01-14 18:39:46 -0600

    Add /hacker_news_encoding route to the web app.

--- hello_app.py
+++ hello_app.py
@@ -1,4 +1,5 @@
 import flask
+import requests
 
 
 app = flask.Flask(__name__)
@@ -7,3 +8,9 @@ app = flask.Flask(__name__)
 @app.route("/hello")
 def hello():
     return "hello world"
+
+
+@app.route("/hacker_news_encoding")
+def hacker_news_encoding():
+    response = requests.get("https://news.ycombinator.com")
+    return response.headers["Content-Encoding"]
